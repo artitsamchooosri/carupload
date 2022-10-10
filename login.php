496c0847c3cbed940d86df6580645fcbeb6359bf
artitsamchooosri <33492904+artitsamchooosri@users.noreply.github.com>
2022-10-10 11:24:47 +0700

    master

---
+++ login.php
@@ -0,0 +1,88 @@
+<?php
+//Make sure users is not already logged in
+session_start();
+
+if(isset($_SESSION['user_id']))
+{
+    header("Location: ".$results['role']."/index.php");
+}
+
+//Get Database
+require("config.php");
+
+
+//Empty error
+$message = '';
+  
+//Check to make sure that the users information was submitted to the form and not empty
+if(!empty($_POST['email']) && !empty($_POST['password']))
+{
+    //Stores result from database
+    $records = $conn->prepare('SELECT idx,email,password,role FROM users WHERE email = :email');
+    
+    //Gets email from database
+    $records->bindParam(':email', $_POST['email']); 
+    $records->execute();
+    
+    //Gets email associative array
+    $results = $records->fetch(PDO::FETCH_ASSOC);
+    
+    //If there are results, and the password inputed matches, log the user in.
+    if(count($results) > 0 && password_verify($_POST['password'], $results['password'])) //Password verify matches the hashed password against result.
+    {
+        //Once succesfully logged in, set session and redirect
+        $_SESSION['user_id'] = $results['idx'];
+        $_SESSION['role']=$results['role'];
+        header("Location: ".$results['role']."/index.php");
+    }
+    else
+    {
+        
+        $message = 'Sorry, those credentials do not match';
+    }
+    
+}
+
+
+    
+?>
+
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>Login Below</title>
+  <link rel="stylesheet" href="assets/css/style.css" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Comfortaa" rel="stylesheet">
+  </head>
+  <body>
+      
+      <div class="header">
+          
+          <a href="user/index.php">Web App</a>
+      
+      </div>
+      
+      <?php if(!empty($message)): ?>
+      <p><?= $message ?></p>
+      <?php endif; ?>
+      
+      <h1>Login </h1>
+      <span>or <a href="register.php">Register here</a></span>
+      
+      <form action="login.php" method="POST">
+          
+          <input type="email" placeholder="Enter your email" name="email">
+          
+          <input type="password" placeholder="Enter your password" name="password">
+          
+          <input type="submit">
+      
+      
+      </form>
+
+  
+  </body>
+</html>
---
+++ user/index.php
@@ -0,0 +1,60 @@
+<?php
+
+session_start();
+
+require '../config.php';
+
+
+if(isset($_SESSION['user_id']))
+{
+    //Stores result from database
+    
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>Web App Login</title>
+  <link rel="stylesheet" href="assets/css/style.css" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Comfortaa" rel="stylesheet">
+  </head>
+  <body>
+      
+      <div class="header">
+          
+          <a href="index.php">Web App</a>
+      
+      </div>
+      
+      <br>
+      
+      <?php 
+      
+      if(isset($_SESSION['user_id']))
+      {
+        echo "Welcome, You are Logged in. USER";
+          
+        echo "<br><br><a href='../logout.php'>Logout?</a>";
+      }
+      
+      else
+      {
+          
+          echo "<h1>Please Login or Register</h1>
+          <a href='../login.php'>Login</a> or
+          <a href='../register.php'>Register</a>";
+      }
+    
+    
+      ?>
+    
+      
+  
+  
+  </body>
+</html>
